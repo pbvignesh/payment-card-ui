a512f25f243e9bedec193604d62145442c27aefb
Vignesh Pb <vignesh.pattalam@zoomrx.com>
2023-09-30 21:19:58 +0530

    Update Design of the back of the card

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+var cardNumberCurrentState = '';
+
 function rotate(){
-    let paymentCard=document.getElementById('container');
-    var flip="transform: rotateY(180deg);" 
+    let paymentCard = document.getElementById('container');
+    var flip = "transform: rotateY(180deg);" 
     paymentCard.setAttribute('style', flip);
-    console.log("I'm called");
+}
+
+function rotateBack(){
+    let paymentCard = document.getElementById('container');
+    var flip = "transform: rotateY(0deg);"
+    paymentCard.setAttribute('style', flip);
+}
+
+function updateCardNumber() {
+    let cardInput = document.getElementsByClassName('form-card-number-input')[0].value;
+    if (cardInput == '') {
+        cardNumberCurrentState = '#### #### ####';
+        return;
+    }
+
+    let countOfCardInput = cardInput.toString().length;
+    let countOfCurrentState = cardNumberCurrentState.toString().length;
+    console.log('Count of Card Input' + countOfCardInput.toString());
+    console.log('Count of current state ' + countOfCurrentState);
+    if (countOfCardInput > countOfCurrentState) {
+        console.log('Need to add the last character');
+        cardNumberCurrentState += cardInput[countOfCardInput - 1];
+    } else if (countOfCardInput < countOfCurrentState) {
+        console.log('Need to remove the last character');
+        cardNumberCurrentState = cardNumberCurrentState.slice(0, -1);
+    }
 }
